2d944cba5ab179eb56c232ddd537085070075637
tmitboules <88787579+tmitboules@users.noreply.github.com>
2022-05-31 14:00:53 -0700

    adding ISR to project and testing

--- pages/issues/[issuePage].tsx
+++ pages/issues/[issuePage].tsx
@@ -1,26 +1,61 @@
 import type { GetStaticPaths, GetStaticProps, NextPage } from "next";
 import client from "../../apollo-client";
 import ISSUES_COUNT_QUERY from "../../queries/issues/count.graphql";
-import { IssueConnection } from "../../src/generated/graphql";
+import PAGINATED_ISSUES from "../../queries/issues/paginatedIssues.graphql";
+import { Issue, IssueConnection } from "../../src/generated/graphql";
 import { createArrayOfPages } from "../../utils";
 
-interface Props {}
+const ISSUES_PER_PAGE = 10;
 
-const PaginatedIssues: NextPage<Props> = ({ page }: any) => {
+interface Props {
+  issues: Issue[];
+  issuesConnection: IssueConnection;
+}
+
+const PaginatedIssues: NextPage<Props> = ({ issues, issuesConnection }) => {
   return (
     <div>
-      <h1>hello PaginatedIssues page:{page}</h1>
+      <h1>hello PaginatedIssues page:{issuesConnection.aggregate.count}</h1>
+      <div>
+        {issues.map((issue) => (
+          <div key={issue.publicationDate}>
+            <h1>Date: {issue.publicationDate}</h1>
+            <h3>Issue Number: {issue.issueNumber}</h3>
+            <a
+              href={issue.issueFile.url}
+              download
+              target="_blank"
+              rel="noreferrer"
+            >
+              link to issue
+            </a>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
 
 export default PaginatedIssues;
 
-export const getStaticProps: GetStaticProps = async ({ params }) => {
+export const getStaticProps: GetStaticProps<Props> = async ({ params }) => {
+  const skip: number =
+    (parseInt(params?.issuePage as string) - 1) * ISSUES_PER_PAGE;
+
+  const { data }: { data: Props } = await client.query({
+    query: PAGINATED_ISSUES,
+    variables: {
+      skip: skip,
+      first: ISSUES_PER_PAGE,
+    },
+  });
+
   return {
     props: {
-      page: params?.issuePage,
+      issues: data.issues,
+      issuesConnection: data.issuesConnection,
     },
+    revalidate: 5,
   };
 };
 
@@ -32,7 +67,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
 
   const issuesArray = createArrayOfPages(
     data.issuesConnection.aggregate.count,
-    10
+    ISSUES_PER_PAGE
   );
 
   return {
@@ -41,6 +76,6 @@ export const getStaticPaths: GetStaticPaths = async () => {
         issuePage: issuePage.toString(),
       },
     })),
-    fallback: false,
+    fallback: "blocking",
   };
 };
